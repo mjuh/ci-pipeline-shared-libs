d5cb262c7063b0d68ca7cc8408f987f48e3daa83
sidorov <sidorov@majordomo.ru>
2018-02-21 20:25:44 +0300

    buildAndPushDocker

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -1,6 +1,6 @@
-def call(Map args = [:]) {
+def call(Map args) {
 	def tag = args.tag ?: ${env.BRANCH_NAME}
-	def image = docker.build("my-image:${tag}")	
+	def image = docker.build("${image}:${tag}")	
     docker.withRegistry('https://docker-registry.intr', 'docker-registry') {
         image.push()
     }
