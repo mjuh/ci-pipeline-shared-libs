6253fa457e1886e376e42752fda167b26d58fde6
sidorov <sidorov@majordomo.ru>
2018-02-21 17:54:51 +0300

    map instead of closure

--- vars/echoTest.groovy
+++ vars/echoTest.groovy
@@ -1,10 +1,3 @@
-def call(body) {
-	def config = [:]
-	body.resolveStrategy = Closure.DELEGATE_FIRST
-	body.delegate = config
-	body()
-
-    println config.var1
-    println config.var2
-    println config.var3
+def call(Map parameters) {
+	parameters.each{ k, v -> println "${k}:${v}" }
 }
