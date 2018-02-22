2ab71fca42417c2a8138899f322d40258e798267
sidorov <sidorov@majordomo.ru>
2018-02-22 18:50:11 +0300

    file(PWD)

--- vars/groupName.groovy
+++ vars/groupName.groovy
@@ -2,7 +2,7 @@
 import org.ajoberstar.grgit.Grgit
 
 def call() {
-	def git = Grgit.open(dir: env.PWD)
+	def git = Grgit.open(dir: file(env.PWD))
 	def url = git.remote.list().find { it.name == 'origin' }
 	println url
 }
