7a56e989b5f031f2bde8eb8ce4ae0e9a35c6ec70
sidorov <sidorov@majordomo.ru>
2018-02-22 20:28:04 +0300

    varFromGit

--- vars/varFromGit.groovy
+++ vars/varFromGit.groovy
@@ -1,14 +1,12 @@
 @Grab('org.ajoberstar:grgit:2.0.1')
 import org.ajoberstar.grgit.Grgit
 
-def git = Grgit.open(dir: env.WORKSPACE)
-def url = git.remote.list().find { it.name == 'origin' }.url
-def (project, group) = url.split(':').tail().join(':').split('/|\\.')[-2..0]
-
 def getProject() {
-	return project
+	def url = Grgit.open(dir: env.WORKSPACE).remote.list().find { it.name == 'origin' }.url
+	url.split(':').tail().join(':').split('/|\\.')[-2]
 }
 
 def getGroup() {
-	return group
+	def url = Grgit.open(dir: env.WORKSPACE).remote.list().find { it.name == 'origin' }.url
+	url.split(':').tail().join(':').split('/|\\.')[-3]
 }
