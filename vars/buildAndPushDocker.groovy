6bb62a176c111958ab48205d19ba478315574fa9
sidorov <sidorov@majordomo.ru>
2018-02-22 15:26:25 +0300

    cleanup

--- vars/buildAndPushDocker.groovy
+++
@@ -1,9 +0,0 @@
-def call(Map args) {
-//	node {
-//		docker.withRegistry('https://docker-registry.intr', 'docker-registry') {
-//			def dockerImage = docker.build("${args.image}:${args.tag}")
-//			dockerImage.push()
-//		}
-	println 'noop'
-//	}
-}
