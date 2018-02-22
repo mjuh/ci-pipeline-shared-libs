7e9811d462513190cc6da2259fe95ddc421b1fd7
sidorov <sidorov@majordomo.ru>
2018-02-22 18:01:14 +0300

    grab

---
+++ src/Grabbed.groovy
@@ -0,0 +1,4 @@
+@Grab('org.ajoberstar:grgit:2.0.1')
+import org.ajoberstar.grgit.Grgit
+
+def grgit = Grgit
--- vars/groupName.groovy
+++ vars/groupName.groovy
@@ -1,8 +1,5 @@
-@Grab('org.ajoberstar:grgit:2.0.1')
-import org.ajoberstar.grgit.Grgit
-
 def call() {
-	def git = Grgit.open(file('.'))
+	def git = Grabbed.Grgit.open(file('.'))
 	def url = git.remote.list().find { it.name == 'origin' }
 	println url
 }
