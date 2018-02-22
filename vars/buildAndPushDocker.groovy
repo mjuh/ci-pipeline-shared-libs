3bce114eac550118adc98a3f79c81e43f9ce8c23
sidorov <sidorov@majordomo.ru>
2018-02-22 15:20:59 +0300

    buildAndPushDocker

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -1,9 +1,9 @@
 def call(Map args) {
-	node {
+//	node {
 //		docker.withRegistry('https://docker-registry.intr', 'docker-registry') {
 //			def dockerImage = docker.build("${args.image}:${args.tag}")
 //			dockerImage.push()
 //		}
 	sh 'printenv'
-	}
+//	}
 }
