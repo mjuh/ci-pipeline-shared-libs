22892752e938fc3415e140c1bc3f371ff6a4f2f9
sidorov <sidorov@majordomo.ru>
2018-02-22 16:46:49 +0300

    buildAndPushDocker

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -1,6 +1,15 @@
 def call(Map args) {
-	docker.withRegistry('https://docker-registry.intr', 'docker-registry') {
-		def dockerImage = docker.build("${args.image}:${args.tag}")
+
+	assert args.image : "No image name provided"
+	
+	def tag = args.tag ?: "latest"
+	def dockerfile = args.dockerfile ?: "Dockerfile"
+	def dockerfileDir = args.dockerfileDir ?: "."
+	def registryUrl = args.registryUrl ?: "https://docker-registry.intr"
+	def credentialsId = args.credentialsId ?: "docker-registry"
+
+	docker.withRegistry(registryUrl, credentialsId) {
+		def dockerImage = docker.build("${args.image}:${tag}", "-f ${dockerfile} ${dockerfileDir}")
 		dockerImage.push()
 	}
 }
