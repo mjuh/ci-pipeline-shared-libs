b15f42c0de3a3217afd142a2c0c35878959fa579
sidorov <sidorov@majordomo.ru>
2018-02-22 18:13:40 +0300

    qwerty

--- src/GrgitWrapper.groovy
+++
@@ -1,4 +0,0 @@
-@Grab('org.ajoberstar:grgit:2.0.1')
-import org.ajoberstar.grgit.Grgit
-
-class GrgitWrapper extends Grgit {}
--- vars/groupName.groovy
+++ vars/groupName.groovy
@@ -1,5 +1,8 @@
+@Grab('org.ajoberstar:grgit:2.0.1')
+import org.ajoberstar.grgit.Grgit
+
 def call() {
-	def git = GrgitWrapper.open(file('.'))
+	def git = Grgit.open(dir: '.')
 	def url = git.remote.list().find { it.name == 'origin' }
 	println url
 }
