4cd7cae62d8edc3f93959def574cd7ad26dd50f8
sidorov <sidorov@majordomo.ru>
2018-02-22 21:03:25 +0300

    plain functions

--- vars/GitRemoteOrigin.groovy
+++ vars/GitRemoteOrigin.groovy
@@ -1,17 +1,14 @@
 @Grab('org.ajoberstar:grgit:2.0.1')
 import org.ajoberstar.grgit.Grgit
-import org.ajoberstar.grgit.Remote 
 
-class GitRemoteOrigin {
-    static void main(_) {}
-    static Remote getRemote() {
-        Grgit.open(dir: env.WORKSPACE).remote.list().find { it.name == 'origin' }
-    }
-    static final String getGroup() {
-        this.getRemote().url.split(':').tail().join(':').split('/|\\.')[-3]
-    }
-    static final String getProject() {
-        this.getRemote().url.split(':').tail().join(':').split('/|\\.')[-2]
-    }
+def getRemote() {
+	Grgit.open(dir: env.WORKSPACE).remote.list().find { it.name == 'origin' }
 }
 
+def getProject() {
+	getRemote().url.split(':').tail().join(':').split('/|\\.')[-2]
+}
+
+def getGroup() {
+	getRemote().url.split(':').tail().join(':').split('/|\\.')[-3]
+}
