fe4f6448a4609610e37fb4c1baffc243bc734a0b
sidorov <sidorov@majordomo.ru>
2018-02-27 15:02:42 +0300

    dockerStackDeploy

--- vars/dockerStackDeploy.groovy
+++ vars/dockerStackDeploy.groovy
@@ -1,21 +1,28 @@
-def call(Map args) {
+import groovy.json.JsonSlurper
 
+def call(Map args) {
 	assert args.stack : "No stack name provided"
-	assert args.service : "No service name provided"
 
 	def credentialsId = args.credentialsId ?: "docker-registry"
+	def jsonSlurper = new JsonSlurper()
+	def prodService = jsonSlurper.parseText(sh(returnStdout: true,
+                                               script: "docker service inspect ${args.stack}_${args.service} 2>/dev/null").trim() ?: '[]')
 
+	env.STACK = args.stack
+	env.SERVICE = prodService ? args.service : ''
 	env.NS = args.namespace ?: args.stack
 	env.IMAGE = args.image ?: args.service
 	env.TAG = args.tag ?: "master"
 	env.REGISTRY = args.registry ?: "docker-registry.intr"
 
 	withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: credentialsId,
-                    usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-		sh '''
-			docker login -u ${USERNAME} -p ${PASSWORD} ${REGISTRY}
-			docker pull ${REGISTRY}/${NS}/${IMAGE}:${TAG}
-			printenv
-		'''
+                    usernameVariable: 'REGISTRY_USERNAME', passwordVariable: 'REGISTRY_PASSWORD']]) {
+		sh 'docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD $REGISTRY'
+		if(env.SERVICE) {
+			sh 'docker service update --with-registry-auth --force --image $REGISTRY/$NS/$IMAGE:$TAG $STACK_$SERVICE'
+		} else {
+			git(url: 'git@gitlab.intr:_ci/docker-stacks.git')
+			sh 'docker stack deploy --with-registry-auth -c ./docker-stacks/$STACK.yml $STACK'
+		}
 	}
 }
