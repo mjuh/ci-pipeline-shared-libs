a5408e2864eef69589dc31695811484713d697a2
sidorov <sidorov@majordomo.ru>
2018-02-27 18:43:29 +0300

    Constants

--- src/Constants.groovy
+++ src/Constants.groovy
@@ -1,6 +1,7 @@
-final String dockerRegistryHost = 'docker-registry.intr'
-final String dockerRegistryCredId = 'docker-registry'
-final String dockerImageDefaultTag = 'master'
-final String dockerStacksGitRepoUrl = 'git@gitlab.intr:_ci/docker-stacks.git'
-final String gitCredId = 'd8f04931-9047-413a-80f3-eef23003522c'
+import groovy.transform.Field
 
+@Field final String dockerRegistryHost = 'docker-registry.intr'
+@Field final String dockerRegistryCredId = 'docker-registry'
+@Field final String dockerImageDefaultTag = 'master'
+@Field final String dockerStacksGitRepoUrl = 'git@gitlab.intr:_ci/docker-stacks.git'
+@Field final String gitCredId = 'd8f04931-9047-413a-80f3-eef23003522c'
