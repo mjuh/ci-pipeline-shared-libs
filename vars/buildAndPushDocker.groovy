3dac935f64433b9323de1bc1ac962d5d2db2a811
sidorov <sidorov@majordomo.ru>
2018-02-27 18:45:26 +0300

    Constants

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -3,11 +3,11 @@ def call(Map args) {
 	assert args.image : "No image name provided"
 	assert args.namespace : "No namespace provided"
 	
-	def tag = args.tag ?: Constants.dockerImageDefaultTag
+	def tag = args.tag ?: dockerImageDefaultTag
 	def dockerfile = args.dockerfile ?: "Dockerfile"
 	def dockerfileDir = args.dockerfileDir ?: "."
-	def registryUrl = args.registryUrl ?: "https://" + Constants.dockerRegistryHost
-	def credentialsId = args.credentialsId ?: Constants.dockerRegistryCredId
+	def registryUrl = args.registryUrl ?: "https://" + dockerRegistryHost
+	def credentialsId = args.credentialsId ?: dockerRegistryCredId
 
 	docker.withRegistry(registryUrl, credentialsId) {
 		def dockerImage = docker.build("${args.namespace}/${args.image}:${tag}", "-f ${dockerfile} ${dockerfileDir}")
