cf24fa2a6be36f96a284edd3bae85492ca204e99
sidorov <sidorov@majordomo.ru>
2018-03-02 16:51:45 +0300

    fix docker inspect

--- vars/composer.groovy
+++ vars/composer.groovy
@@ -12,7 +12,7 @@ def call(Map args) {
 	def uid = sh(returnStdout: true, script: 'id -u').trim()
 	def workspaceOnHost = jenkinsHomeOnHost + env.WORKSPACE - env.HOME
 
-	writeFile(file: 'composer-passwd', text: "jenkins:x:${uid}:${uid}:,,,,:/home/jenkins:/bin/bash")
+//	writeFile(file: 'composer-passwd', text: "jenkins:x:${uid}:${uid}:,,,,:/home/jenkins:/bin/bash")
 	sh "mkdir -p $HOME/composer-tmp"
 	sh "cp -R ${srcDir} build"
 
