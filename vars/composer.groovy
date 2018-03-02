d4cc19d599fb2dfe374cffb487fd3944a16f193e
sidorov <sidorov@majordomo.ru>
2018-03-02 16:27:47 +0300

    wut

--- vars/composer.groovy
+++ vars/composer.groovy
@@ -10,17 +10,17 @@ def call(Map args) {
 	def srcDir = args.srcDir ?: 'src'
 
 	def jenkinsHomeOnHost = jenkinsContainer.getMountByDestination(env.HOME).Source
-	def uid = sh(returnStdout: true, script: 'id -u').trim()
-	def workspaceOnHost = jenkinsHomeOnHost + env.WORKSPACE - env.HOME
-	def passwd = new File('./composer-passwd')
+//	def uid = sh(returnStdout: true, script: 'id -u').trim()
+//	def workspaceOnHost = jenkinsHomeOnHost + env.WORKSPACE - env.HOME
+//	def passwd = new File('./composer-passwd')
 
-	passwd << "jenkins:x:${uid}:${uid}:,,,,:/home/jenkins:/bin/bash"
-	sh "mkdir -p $HOME/composer-tmp"
-	sh "cp -R ${srcDir} build"
+//	passwd << "jenkins:x:${uid}:${uid}:,,,,:/home/jenkins:/bin/bash"
+//	sh "mkdir -p $HOME/composer-tmp"
+//	sh "cp -R ${srcDir} build"
 
-	withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: dockerCredId,
-                      usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-		sh "docker login -u $USERNAME -p $PASSWORD ${registry}"
-		sh "docker run --rm --user ${uid}:${uid} -e 'PHP_VERSION=${phpVersion}' -v ./composer-passwd:/etc/passwd:ro -v $HOME/composer-tmp:/composer -v $HOME:/home/jenkins -v ${workspaceOnHost}/build:/app ${composer} ${cmd}"
-	}
+//	withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: dockerCredId,
+//                      usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+//		sh "docker login -u $USERNAME -p $PASSWORD ${registry}"
+//		sh "docker run --rm --user ${uid}:${uid} -e 'PHP_VERSION=${phpVersion}' -v ./composer-passwd:/etc/passwd:ro -v $HOME/composer-tmp:/composer -v $HOME:/home/jenkins -v ${workspaceOnHost}/build:/app ${composer} ${cmd}"
+//	}
 }
