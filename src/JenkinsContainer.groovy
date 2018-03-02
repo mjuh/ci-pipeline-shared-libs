8cda3e3289d026356c9afffc970f58bdf2870ea0
sidorov <sidorov@majordomo.ru>
2018-03-02 18:59:06 +0300

    fix docker inspect

--- src/JenkinsContainer.groovy
+++ src/JenkinsContainer.groovy
@@ -1,13 +1,14 @@
 import groovy.json.JsonSlurperClassic
 
 class JenkinsContainer {
+	private Map info = new groovy.json.JsonSlurperClassic().parseText(
+		"docker inspect ${Constants.jenkinsContainerName}".execute().text.trim()
+	)
 	public getInfo() {
-		new groovy.json.JsonSlurperClassic().parseText(
-			"docker inspect ${Constants.jenkinsContainerName}".execute().text.trim()
-		)
+		this.info
 	}
 	public getMountByDestination(String destination) {
-		this.getInfo()[0].Mounts.find { it.Destination == destination }
+		this.info[0].Mounts.find { it.Destination == destination }
 	}
 }
 
