446e95b21061bb0c272e633df888ccb80e0b250b
sidorov <sidorov@majordomo.ru>
2018-03-02 17:28:54 +0300

    fix docker inspect

--- vars/jenkinsContainer.groovy
+++ vars/jenkinsContainer.groovy
@@ -5,12 +5,9 @@ def jsonParse(def json) {
     new groovy.json.JsonSlurperClassic().parseText(json)
 }
 
-@NonCPS
-def getInfo() {
-	jsonParse(sh(returnStdout: true,
-                 script: "docker inspect ${Constants.jenkinsContainerName}").trim())
-}
-
 def getMountByDestination(String destination) {
-	getInfo()[0].Mounts.find { it.Destination == destination }
+	jsonParse(
+		sh(returnStdout: true,
+		   script: "docker inspect ${Constants.jenkinsContainerName}").trim()
+	).[0].Mounts.find { it.Destination == destination }
 }
