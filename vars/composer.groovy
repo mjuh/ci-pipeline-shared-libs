a4511e04abe34439a2458932cd62ccfa9e7fa42c
sidorov <sidorov@majordomo.ru>
2018-03-05 20:01:17 +0300

    composer

--- vars/composer.groovy
+++ vars/composer.groovy
@@ -5,30 +5,32 @@ def call(Map args) {
 	def composerNs = args.composerDockerNamespace ?: Constants.composerDockerNamespace 
 	def composerImage = args.composerDockerImage ?: Constants.composerDockerImage
 	def composerTag = args.composerDockerTag ?: Constants.composerDockerTag
-	def composer = "${registry}/${composerNs}/${composerImage}:${composerTag}"
 	def phpVersion = args.phpVersion ?: 'php56'
 	def srcDir = args.srcDir ?: 'src'
 	def jenkinsHomeOnHost = new JenkinsContainer().getMountByDestination(env.HOME).Source
 	def uid = sh(returnStdout: true, script: 'id -u').trim()
 	def workspaceOnHost = jenkinsHomeOnHost + (env.WORKSPACE - env.HOME)
 
+	sh "mkdir -p $HOME/composer/home"
+	createSshDirWithGitKey(dir: env.WORKSPACE + '.ssh')
 	writeFile(file: 'composer-passwd', text: "jenkins:x:${uid}:${uid}:,,,,:/home/jenkins:/bin/bash\n")
 	writeFile(file: 'composer-group', text: "jenkins:x:${uid}:jenkins\n")
-	sh "mkdir -p $HOME/composer/home"
 	sh "cp -pR $WORKSPACE/${srcDir} $WORKSPACE/build"
 
 	withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: dockerCredId,
                       usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
 		sh "docker login -u $USERNAME -p $PASSWORD ${registry}"
-		sh """docker run --rm --name composer						\
+		sh """
+			docker run --rm --name composer							\
 			--user ${uid}:${uid}									\
 			-e 'PHP_VERSION=${phpVersion}'							\
 			-e 'COMPOSER_HOME=/composer/home'						\
 			-v ${workspaceOnHost}/composer-passwd:/etc/passwd:ro	\
 			-v ${workspaceOnHost}/composer-group:/etc/group:ro		\
+			-v ${workspaceOnHost}/jenkins_home:/home/jenkins		\
 			-v ${jenkinsHomeOnHost}/composer:/composer				\
-			-v ${jenkinsHomeOnHost}:/home/jenkins					\
 			-v ${workspaceOnHost}/build:/app						\
-			${composer} ${cmd}"""
+			${registry}/${composerNs}/${composerImage}:${composerTag} ${cmd}
+		"""
 	}
 }
---
+++ vars/createSshDirWithGitKey.groovy
@@ -0,0 +1,34 @@
+def call(Map args) {
+	def gitCredId = args.gitCredId ?: Constants.gitCredId
+	def gitHost = args.gitHost ?: Constants.gitHost
+	def localUsername = args.localUsername ?: 'jenkins'
+	def localHomedir = args.localHomedir ?: env.HOME
+	def dir = args.dir ?: '.ssh'
+	def sshWrapperFilename = args.sshWrapperFilename ?: 'wrap-ssh4git.sh'
+
+	withCredentials([[$class: 'sshUserPrivateKey', credentialsId: gitCredId,
+                      usernameVariable: 'USERNAME', keyFileVariable: 'KEY_FILE']]) {
+		sh "mkdir -p -m 700 ${dir}"
+		writeFile(
+			file: dir + '/config',
+			text: """
+				Host ${gitHost}
+				User ${USERNAME}
+				HostName ${gitHost}
+				IdentityFile ${localHomedir}/${dir}/git_repos_deploy_key
+			"""
+		)
+		writeFile(
+			file: dir + '/' + sshWrapperFilename,
+			text: """
+				#!/bin/sh
+				/usr/bin/env ssh -o 'StrictHostKeyChecking=no' -i '${localHomedir}/${dir}/git_repos_deploy_key' $1 $2
+			"""
+		)
+		sh """
+			chmod +x ${dir}/${sshWrapperFilename}
+			cp $KEY_FILE ${dir}/git_repos_deploy_key
+			chmod 400 ${dir}/git_repos_deploy_key
+		"""
+	}
+}
