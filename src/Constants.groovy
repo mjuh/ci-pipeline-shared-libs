3519576d89b841db6fdfa2500c5bbab19de38802
sidorov <sidorov@majordomo.ru>
2018-03-06 19:35:38 +0300

    dockerRun

--- src/Constants.groovy
+++ src/Constants.groovy
@@ -1,18 +1,17 @@
 public class Constants {
-	def main (_) {}
-	public static final String dockerRegistryHost = 'docker-registry.intr'
-	public static final String dockerRegistryCredId = 'docker-registry'
-	public static final String dockerImageDefaultTag = 'master'
+    def main (_) {}
+    public static final String dockerRegistryHost = 'docker-registry.intr'
+    public static final String dockerRegistryCredId = 'docker-registry'
+    public static final String dockerImageDefaultTag = 'master'
 
-	public static final String dockerStacksGitRepoUrl = 'git@gitlab.intr:_ci/docker-stacks.git'
-	public static final String dockerStacksDeployDir = 'docker-stacks'
+    public static final String dockerStacksGitRepoUrl = 'git@gitlab.intr:_ci/docker-stacks.git'
+    public static final String dockerStacksDeployDir = 'docker-stacks'
 
-	public static final String gitCredId = 'd8f04931-9047-413a-80f3-eef23003522c'
-	public static final String gitHost = 'gitlab.intr'
+    public static final String gitCredId = 'd8f04931-9047-413a-80f3-eef23003522c'
+    public static final String gitHost = 'gitlab.intr'
 
-	public static final String jenkinsContainerName = 'jenkins'
+    public static final String jenkinsContainerName = 'jenkins'
 
-	public static final String composerDockerNamespace = 'base'
-	public static final String composerDockerImage = 'mj-composer'
-	public static final String composerDockerTag = 'master'
+    public static final String composerDockerImage = dockerRegistryHost + 'base/mj-composer'
+    public static final String composerDockerTag = ':master'
 }
