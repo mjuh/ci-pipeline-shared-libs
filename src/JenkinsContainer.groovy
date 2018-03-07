4e4a335d9fbc8e153f5c27cae786b6cc61049afa
sidorov <sidorov@majordomo.ru>
2018-03-07 14:58:22 +0300

    jenkinsContainer

--- src/JenkinsContainer.groovy
+++ src/JenkinsContainer.groovy
@@ -11,7 +11,7 @@ class JenkinsContainer {
 
     public getHostPath(String path) {
         def hostPath = null
-        def mount = this.info.Mounts.find { it.Destination.startsWith(path) }
+        def mount = this.info.Mounts.find { path.startsWith(it.Destination) }
         if(mount) {
             hostPath = mount.Source + path - mount.Destination
         }
