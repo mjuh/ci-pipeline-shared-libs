a90a99b3a6128472c5e81dfe741bc360bcfff427
sidorov <sidorov@majordomo.ru>
2018-03-07 14:51:20 +0300

    dockerRun

--- src/JenkinsContainer.groovy
+++ src/JenkinsContainer.groovy
@@ -1,12 +1,25 @@
 import groovy.json.JsonSlurperClassic
 
 class JenkinsContainer {
-	private Map info = new groovy.json.JsonSlurperClassic().parseText(
-		"docker inspect ${Constants.jenkinsContainerName}".execute().text.trim()
-	)[0]
+    private Map info = new groovy.json.JsonSlurperClassic().parseText(
+        "docker inspect ${Constants.jenkinsContainerName}".execute().text.trim()
+    )[0]
 
-	public getMountByDestination(String destination) {
-		this.info.Mounts.find { it.Destination == destination }
-	}
+    public getMountByDestination(String destination) {
+        this.info.Mounts.find { it.Destination == destination }
+    }
+
+    public getHostPath(String path) {
+        def hostPath = null
+        def mount = this.info.find { it.Destination.startsWith(path) }
+        if(mount) {
+            hostPath = mount.Source + path - mount.Destination
+        }
+        hostPath
+    }
+
+    public getUid() {
+        "id -u".execute().text
+    }
 }
 
--- vars/composer.groovy
+++ vars/composer.groovy
@@ -4,10 +4,11 @@ def call(Map args = [:]) {
     def composerTag = args.composerTag ?: Constants.composerDockerTag
     def phpVersion = args.phpVersion ?: 'php56'
     def srcDir = args.srcDir ?: 'src'
-    def jenkinsHomeOnHost = new JenkinsContainer().getMountByDestination(env.HOME).Source
-    def jenkinsHomeInContainer = '/home/jenkins'
-    def workspaceOnHost = jenkinsHomeOnHost + (env.WORKSPACE - env.HOME)
-    def uid = sh(returnStdout: true, script: 'id -u').trim()
+    def jenkinsContainer = new JenkinsContainer()
+    def homeOnHost = jenkinsContainer.getHostPath(env.HOME)
+    def workspaceOnHost = jenkinsContainer.getHostPath(env.WORKSPACE)
+    def uid = jenkinsContainer.getUid()
+    def jenkinsHomeInComposerContainer = '/home/jenkins'
 
     echo 'Copying src/ to build/'
     sh "cp -a $WORKSPACE/${srcDir}/. $WORKSPACE/build"
@@ -18,23 +19,23 @@ def call(Map args = [:]) {
     sh "mkdir -p $HOME/composer/home"
 
     echo '... creating passwd and group files with single user `jenkins`'
-    writeFile(file: 'passwd', text: "jenkins:x:${uid}:${uid}:,,,,:${jenkinsHomeInContainer}:/bin/sh\n")
+    writeFile(file: 'passwd', text: "jenkins:x:${uid}:${uid}:,,,,:${jenkinsHomeInComposerContainer}:/bin/sh\n")
     writeFile(file: 'group', text: "jenkins:x:${uid}:jenkins\n")
 
     echo '... creating .ssh with config, wrapper and key needed for `git clone`'
     createSshDirWithGitKey(dir: env.WORKSPACE + '/jenkins_home/.ssh',
-                           inConfigDir: jenkinsHomeInContainer,
+                           inConfigDir: jenkinsHomeInComposerContainer,
                            sshWrapperFilename: 'ssh_wrapper.sh')
 
     echo 'Running Docker container'
     dockerRun(volumes: ["${workspaceOnHost}/passwd": '/etc/passwd',
                         "${workspaceOnHost}/group": '/etc/group',
-                        "${workspaceOnHost}/jenkins_home": jenkinsHomeInContainer,
-                        "${jenkinsHomeOnHost}/composer": '/composer',
+                        "${workspaceOnHost}/jenkins_home": jenkinsHomeInComposerContainer,
+                        "${homeOnHost}/composer": '/composer',
                         "${workspaceOnHost}/build": '/app'],
               env: [PHP_VERSION: phpVersion,
                     COMPOSER_HOME: '/composer/home',
-                    GIT_SSH: "${jenkinsHomeInContainer}/.ssh/ssh_wrapper.sh"],
+                    GIT_SSH: "${jenkinsHomeInComposerContainer}/.ssh/ssh_wrapper.sh"],
               image: "${composerImage}:${composerTag}",
               name: "composer-${env.BUILD_TAG}",
               cmd: cmd)
