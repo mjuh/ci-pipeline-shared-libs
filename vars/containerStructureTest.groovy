ae168875f2bb307520aa38fdca24c6ac4236ee86
sidorov <sidorov@majordomo.ru>
2018-03-13 18:50:18 +0300

    buildDocker

--- vars/containerStructureTest.groovy
+++ vars/containerStructureTest.groovy
@@ -5,7 +5,7 @@ def call(Map args = [:]) {
     }
     def registry = args.registry ?: Constants.dockerRegistryHost
     def tag = args.tag ?: Constants.dockerImageDefaultTag
-    def imageName = args.image ? image.imageName() : "${registry}/${args.namespace}/${args.name}"
+    def imageName = args.image ? args.image.imageName() : "${registry}/${args.namespace}/${args.name}"
     def containerStructureTestImage = args.containerStructureTestImage ?: Constants.containerStructureTestImage
     def config = args.configFileName ?: 'container-structure-test.yaml'
     assert config.endsWith('.yaml') || config.endsWith('.json') : 'Supported config file extensions are .yaml and .json'
