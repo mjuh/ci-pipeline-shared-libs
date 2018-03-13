800a8d444beb7df7524c68b5f402694a211d5fa4
sidorov <sidorov@majordomo.ru>
2018-03-13 19:20:17 +0300

    buildGenericDockerProject

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,7 +1,12 @@
-def call(msg, status = 'good') {
+def call(msg, color = 'green') {
+    def rgb = [green: '#00ff00',
+               yellow: '#ffff00',
+               red: '#ff0000',
+               good: '#00ff00',
+               bad: '#ff0000'][color.toLowerCase()]
     slackSend(botUser: true,
               channel: Constants.slackChannel,
-              color: status,
+              color: rgb,
               message: msg,
               teamDomain: Constants.slackTeam,
               token: Constants.slackToken)
