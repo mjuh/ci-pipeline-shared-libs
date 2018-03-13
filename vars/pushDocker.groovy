6af311f9968d0fe69b90684852d096f807e6542a
sidorov <sidorov@majordomo.ru>
2018-03-13 17:57:25 +0300

    buildDocker

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -1,3 +1,10 @@
-def call(image) {
-    image.push()
+def call(Map args = [:]) {
+    assert args.image : "No image provided"
+
+    def registryUrl = args.registryUrl ?: "https://" + Constants.dockerRegistryHost
+    def credentialsId = args.credentialsId ?: Constants.dockerRegistryCredId
+
+    docker.withRegistry(registryUrl, credentialsId) {
+        image.push()
+    }
 }
