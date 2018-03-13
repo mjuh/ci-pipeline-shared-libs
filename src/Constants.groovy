133ef75b4bee520ba06d24c4d79093cbfe0da50a
sidorov <sidorov@majordomo.ru>
2018-03-13 17:38:41 +0300

    gitlab

--- src/Constants.groovy
+++ src/Constants.groovy
@@ -4,7 +4,7 @@ public class Constants {
     public static final String dockerRegistryCredId = 'docker-registry'
     public static final String dockerImageDefaultTag = 'master'
 
-    public static final String dockerStacksGitRepoUrl = 'git@notifyGitlab.intr:_ci/docker-stacks.git'
+    public static final String dockerStacksGitRepoUrl = 'git@gitlab.intr:_ci/docker-stacks.git'
     public static final String dockerStacksDeployDir = 'docker-stacks'
 
     public static final String gitCredId = 'd8f04931-9047-413a-80f3-eef23003522c'
