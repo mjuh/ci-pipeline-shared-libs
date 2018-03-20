3205d892dd9089528455abe9fdbdce5fe946ca64
sidorov <sidorov@majordomo.ru>
2018-03-20 20:10:47 +0300

    trow out weed

--- vars/notifyGitlab.groovy
+++
@@ -1,9 +0,0 @@
-def call(String state, String name = STAGE_NAME) {
-    def gitlabState = [running: 'running',
-                       success: 'success',
-                       failed: 'failed',
-                       failure: 'failed',
-                       canceled: 'canceled',
-                       aborted: 'canceled'][state]
-    updateGitlabCommitStatus(name: name, state: gitlabState)
-}
