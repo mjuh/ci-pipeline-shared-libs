2ae6caa91778f56d22d29e94d7380a4a29a9a194
sidorov <sidorov@majordomo.ru>
2018-03-27 16:35:08 +0300

    dockerPull as separate step + dockerStackDeploy refactored

---
+++ vars/dockerPull.groovy
@@ -0,0 +1,32 @@
+@NonCPS
+def getNodeNames(String label) {
+    jenkins.model.Jenkins.instance.nodes
+            .findAll { node -> node.labelString == label }
+            .collect { node -> node.name }
+}
+
+def call(Map args = [:]) {
+    assert args.image : "No Image provided"
+    def imageName = args.image.imageName()
+    def credentialsId = args.credentialsId ?: Constants.dockerRegistryCredId
+    def registry = args.registry ?: Constants.dockerRegistryHost
+    def nodeLabel = args.nodeLabel ?: Constants.productionNodeLabel
+
+    def nodes = [:]
+    def names = getNodeNames(nodeLabel)
+    for (int i=0; i<names.size(); ++i) {
+        def nodeName = names[i];
+        nodes[nodeName] = {
+            node(nodeName) {
+                withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: credentialsId,
+                                  usernameVariable: 'REGISTRY_USERNAME', passwordVariable: 'REGISTRY_PASSWORD']]) {
+                    sh """
+                        docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD ${registry}
+                        docker pull ${imageName}
+                    """
+                }
+            }
+        }
+    }
+    parallel nodes
+}
