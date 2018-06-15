a05606fe4ddd6e4bdde7e6c1100faf3b7031afcc
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2018-06-15 17:28:35 +0300

    filesDeploy test

---
+++ vars/filesDeploy.groovy
@@ -0,0 +1,31 @@
+@NonCPS
+def getNodeNames(String label) {
+    jenkins.model.Jenkins.instance.nodes
+            .findAll { node -> node.labelString == label }
+            .collect { node -> node.name }
+}
+
+def call(Map args = [:]) {
+    assert args.srcPath : "No source path provided"
+    assert args.dstPath: "No destinaion path provided"
+    assert args.nodeLabel : "No node label provided"
+    stashName = 'transfer'
+
+    dir(args.srcPath){
+        stash(name: stashName)
+    }
+
+    def nodes = [:]
+    def names = getNodeNames(nodeLabel)
+    for (int i=0; i<names.size(); ++i) {
+        def nodeName = names[i];
+        nodes[nodeName] = {
+            node(nodeName) {
+                dir(args.dstPath) {
+                    unstash(name: stashName)
+                }
+            }
+        }
+    }
+    parallel nodes
+}
