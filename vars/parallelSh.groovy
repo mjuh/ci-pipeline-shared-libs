697266f542179f00db4045388d460ed336dfcf08
sidorov <sidorov@majordomo.ru>
2018-06-18 18:03:53 +0300

    lowercase variable

--- vars/parallelSh.groovy
+++ vars/parallelSh.groovy
@@ -6,7 +6,7 @@ def getNodeNames(List<String> labels) {
 }
 
 def call(Map args = [:]) {
-    assert args.Cmd: "No command provided"
+    assert args.cmd: "No command provided"
     assert args.nodeLabels : "No node labels provided"
     assert args.nodeLabels instanceof List<String> : "Node labels should be a list of strings"
 
@@ -16,7 +16,7 @@ def call(Map args = [:]) {
         def nodeName = names[i];
         nodes[nodeName] = {
             node(nodeName) {
-                sh args.Cmd
+                sh args.cmd
             }
         }
     }
