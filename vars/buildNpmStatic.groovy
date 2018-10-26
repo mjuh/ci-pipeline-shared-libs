e6451bc07918ba1341da26b45a50dfab0f72c1b8
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2018-10-26 15:27:51 +0300

    1

--- vars/buildNpmStatic.groovy
+++ vars/buildNpmStatic.groovy
@@ -10,45 +10,45 @@ def call(String dstpath) {
             gitLabConnection(Constants.gitLabConnection)
             gitlabBuilds(builds: ['npm operations', 'stash', 'unstash'])
         }
-    stages {
-        stage('npm operations') {
-          when { branch 'master' }
-            steps {
-                gitlabCommitStatus(STAGE_NAME) {
-                    sh 'npm install'
-                    sh 'npm run-script build'
-                    sh 'npm run-script build-test'
-                }
-            }
-        }
-        stage('stash') {
-          when { branch 'master' }
-            steps {
-                gitlabCommitStatus(STAGE_NAME) {
-                   stash name: "my-stash", includes: "public/*"
-                }
-            }
-        }
-        stage('unstash') {
-          when { branch 'master' }
-          steps {
-             gitlabCommitStatus(STAGE_NAME) {
-                node('dhost-production') {
-                    dir(dstpath) {
-                        unstash "my-stash"
+        stages {
+            stage('npm operations') {
+              when { branch 'master' }
+                steps {
+                    gitlabCommitStatus(STAGE_NAME) {
+                        sh 'npm install'
+                        sh 'npm run-script build'
+                        sh 'npm run-script build-test'
                     }
                 }
-             }
-          }
-          post {
-             success {
-                notifySlack "${PROJECT_NAME} app deployed"
-             }
-          }
+            }
+            stage('stash') {
+              when { branch 'master' }
+                steps {
+                    gitlabCommitStatus(STAGE_NAME) {
+                       stash name: "my-stash", includes: "public/*"
+                    }
+                }
+            }
+            stage('unstash') {
+              when { branch 'master' }
+              steps {
+                 gitlabCommitStatus(STAGE_NAME) {
+                    node('dhost-production') {
+                        dir(dstpath) {
+                            unstash "my-stash"
+                        }
+                    }
+                 }
+              }
+              post {
+                 success {
+                    notifySlack "${PROJECT_NAME} deployed"
+                 }
+              }
+            }
         }
-    }
-    post {
-        success { cleanWs() }
-        failure { notifySlack "Build failled: ${JOB_NAME} [<${RUN_DISPLAY_URL}|${BUILD_NUMBER}>]", "red" }
-    }
+        post {
+            success { cleanWs() }
+            failure { notifySlack "Build failled: ${JOB_NAME} [<${RUN_DISPLAY_URL}|${BUILD_NUMBER}>]", "red" }
+        }
 }
