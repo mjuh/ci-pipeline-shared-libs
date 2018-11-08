60d6779018c7793b0cbcab7f738bd662296fc4dc
Артём Моносов <monosov@majordomo.ru>
2018-11-08 15:50:02 +0300

    use HOSTNAME env var instead of jenkins

--- src/Constants.groovy
+++ src/Constants.groovy
@@ -12,7 +12,6 @@ public class Constants {
 
     public static final String gitLabConnection = 'gitlab.intr'
 
-    public static final String jenkinsContainerName = 'jenkins'
     public static final String productionNodeLabel = 'dhost-production'
 
     public static final String composerDockerImage = "${dockerRegistryHost}/base/mj-composer"
