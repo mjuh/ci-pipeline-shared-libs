416ee808f8c9c118e9c1ce5ed03aa589de62c6f3
sidorov <sidorov@majordomo.ru>
2019-04-03 20:12:59 +0300

    build dockers with nix

---
+++ vars/nixBuildDocker.groovy
@@ -0,0 +1,19 @@
+def call(Map args = [:]) {
+    if(!args.imageName) {
+        assert args.name : "No image name provided"
+        assert args.namespace : "No namespace provided"
+    }
+
+    def imageName = args.imageName ?: "${args.namespace}/${args.name}"
+    def tag = args.tag ?: Constants.dockerImageDefaultTag
+    def registryUrl = args.registryUrl ?: "https://" + Constants.dockerRegistryHost
+    def credentialsId = args.credentialsId ?: Constants.dockerRegistryCredId
+
+    docker.withRegistry(registryUrl, credentialsId) {
+        sh '. /var/jenkins_home/.nix-profile/etc/profile.d/nix.sh && docker load --input \$(nix-build --cores 8 default.nix --show-trace)'
+        sh 'tar xzf result manifest.json'
+        def image = docker.image(readJSON(file: 'manifest.json')[0].repoTags)
+        image.tag("${imageName}:${tag}")
+        image
+    }
+}
