2ef7a35bf8c9a13b036ef1495409bc1c3c5eb4f1
sidorov <sidorov@majordomo.ru>
2019-04-23 19:38:53 +0300

    move env vars outside of non-cps function

--- vars/nexusRawUpload.groovy
+++ vars/nexusRawUpload.groovy
@@ -3,22 +3,20 @@ import java.io.File
 import groovyx.net.http.*
 import static groovyx.net.http.MultipartContent.multipart
 
-def upload(File file, String repoPath, String name) {
-    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: Constants.nexusCredId,
-                      usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD']]) {
-        HttpBuilder.configure {
-            request.uri = Constants.nexusUrl
-            request.auth.basic env.NEXUS_USERNAME, env.NEXUS_PASSWORD
-        }.put {
-            request.uri.path = "/repository/${repoPath}/${name}"
-            request.contentType = 'multipart/form-data'
-            request.body = multipart {
-                part '', '', 'application/octet-stream', file
-            }
-            request.encoder 'multipart/form-data', CoreEncoders.&multipart
+@NonCPS
+def upload(File file, String repoPath, String name, String user, String password) {
+    HttpBuilder.configure {
+        request.uri = Constants.nexusUrl
+        request.auth.basic user, password
+    }.put {
+        request.uri.path = "/repository/${repoPath}/${name}"
+        request.contentType = 'multipart/form-data'
+        request.body = multipart {
+            part '', '', 'application/octet-stream', file
         }
-        println("${file} uploaded to ${Constants.nexusUrl}/repository/${repoPath}/${name}")
+        request.encoder 'multipart/form-data', CoreEncoders.&multipart
     }
+    println("${file} uploaded to ${Constants.nexusUrl}/repository/${repoPath}/${name}")
 }
 def call(Map args = [:]) {
     assert args.file : 'No file provided'
@@ -27,5 +25,8 @@ def call(Map args = [:]) {
     def version = args.version ?: 'latest'
     def (name, ext) = args.file.split("/")[-1].split("\\.", 2) as List
     def versionedName = name + "-${version}." + ext
-    upload(new File(args.file), repo + group, versionedName)
+    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: Constants.nexusCredId,
+                      usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD']]) {
+        upload(new File(args.file), repo + group, versionedName, env.NEXUS_USER, env.NEXUS_PASSWORD)
+    }
 }
