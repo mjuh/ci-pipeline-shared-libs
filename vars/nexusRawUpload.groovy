030b224aa4f8cb12cf057947f660ee34d8134216
sidorov <sidorov@majordomo.ru>
2019-04-24 15:15:32 +0300

    no multipart

--- vars/nexusRawUpload.groovy
+++ vars/nexusRawUpload.groovy
@@ -1,7 +1,6 @@
 @Grab('io.github.http-builder-ng:http-builder-ng-core:1.0.3')
 import java.io.File
-import groovyx.net.http.*
-import static groovyx.net.http.MultipartContent.multipart
+import groovyx.net.http.HttpBuilder
 
 @NonCPS
 def upload(File file, String repoPath, String name, String user, String password) {
@@ -11,12 +10,11 @@ def upload(File file, String repoPath, String name, String user, String password
     }.put {
         request.uri.path = "/repository/${repoPath}/${name}"
         request.contentType = 'multipart/form-data'
-        request.body = multipart {
-            part '', '', 'application/octet-stream', file
-        }
-        request.encoder 'multipart/form-data', CoreEncoders.&multipart
+        request.body = file
     }
-    println("${file} uploaded to ${Constants.nexusUrl}/repository/${repoPath}/${name}")
+    def url = "${Constants.nexusUrl}/repository/${repoPath}/${name}"
+    println("${file} uploaded to ${url}")
+    url
 }
 def call(Map args = [:]) {
     assert args.file : 'No file provided'
@@ -29,11 +27,9 @@ def call(Map args = [:]) {
     if(!filePath.startsWith('/')) {
         filePath = pwd() + "/${filePath}"
     }
-    File file = new File(filePath)
-    println(pwd())
     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: Constants.nexusCredId,
                       usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD']]) {
 
-        upload(file, repo + group, versionedName, env.NEXUS_USER, env.NEXUS_PASSWORD)
+        upload(new File(filePath), repo + group, versionedName, env.NEXUS_USER, env.NEXUS_PASSWORD)
     }
 }
