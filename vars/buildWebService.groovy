bf53417658be9f303ce53273c298bee39415c473
sidorov <sidorov@majordomo.ru>
2019-07-05 18:54:51 +0300

    buildWebService

---
+++ vars/buildWebService.groovy
@@ -0,0 +1,36 @@
+def call() {
+    def dockerImage = null
+
+    pipeline {
+        agent { label 'master' }
+        environment {
+            PROJECT_NAME = gitRemoteOrigin.getProject()
+            GROUP_NAME = gitRemoteOrigin.getGroup()
+        }
+        options {
+            gitLabConnection(Constants.gitLabConnection)
+            gitlabBuilds(builds: ['Build Docker image', 'Push Docker image'])
+            buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        }
+        stages {
+            stage('Build Docker image') {
+                steps {
+                    gitlabCommitStatus(STAGE_NAME) {
+                        script { dockerImage = nixBuildDocker namespace: GROUP_NAME, name: PROJECT_NAME, tag: BRANCH_NAME }
+                    }
+                }
+            }
+            stage('Push Docker image') {
+                steps {
+                    gitlabCommitStatus(STAGE_NAME) {
+                        pushDocker image: dockerImage
+                    }
+                }
+            }
+        }
+        post {
+            success { cleanWs() }
+            failure { notifySlack "Build failled: ${JOB_NAME} [<${RUN_DISPLAY_URL}|${BUILD_NUMBER}>]", "red" }
+        }
+    }
+}
