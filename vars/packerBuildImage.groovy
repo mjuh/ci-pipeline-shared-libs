de18f10377a0ffd58b996200fba2873b347ac5ed
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-08-19 17:17:14 +0300

    [:]

--- vars/packerBuildImage.groovy
+++ vars/packerBuildImage.groovy
@@ -1,10 +1,12 @@
-def call(Map args) {
+def call(Map args = [:]) {
         assert args.template : "No template provided"
         assert args.output : "No output provided"
         assert args.vars : "No packervars provided"
-    def template = args.template
-    def output = args.output
-    def vars = args.vars
+
+        def template = args.template
+        def output = args.output
+        def vars = args.vars
+
     sh '''
         . /home/jenkins/.nix-profile/etc/profile.d/nix.sh ;
         packer build -force -var-file=vars/${vars}.json templates/${template}.json
