f2166b9ef4790678a168329ec542688a359f19e7
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-08-19 17:03:33 +0300

    add packerBuildImage test

---
+++ vars/packerBuildImage.groovy
@@ -0,0 +1,13 @@
+def call(Map args) {
+        assert args.template : "No template provided"
+        assert args.output : "No output provided"
+        assert args.vars : "No packervars provided"
+    def template = args.template
+    def output = args.output
+    def vars = args.vars
+    sh '''
+        . /home/jenkins/.nix-profile/etc/profile.d/nix.sh ;
+        packer build -force -var-file=vars/${vars}.json templates/${template}.json
+        ls -alah */${output}
+    '''
+}
