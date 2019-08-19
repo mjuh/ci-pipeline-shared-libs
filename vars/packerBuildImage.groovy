dcc8e6037f498d1b116bbb9d15423b01da0bf056
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-08-19 17:46:54 +0300

    upload

--- vars/packerBuildImage.groovy
+++ vars/packerBuildImage.groovy
@@ -6,7 +6,9 @@ def call(Map args = [:]) {
         def template = args.template
         def output = args.output
         def vars = args.vars
+        def upload = args.upload ?: false
 
+    if(!upload){
     sh """
         echo "vars: ${vars} templ: ${template} output: ${output}"
         echo "args.vars: ${args.vars} args.templ: ${args.template} args.output: ${args.output}"
@@ -15,4 +17,23 @@ def call(Map args = [:]) {
         ls -alah */${output}
 
     """
+    }
+    if(upload){
+        if(env.BRANCH_NAME == "master"){
+            sh """
+                . /home/jenkins/.nix-profile/etc/profile.d/nix.sh ;
+                packer build -force -var-file=vars/${vars}.json templates/${template}.json
+                ls -alah */${output}
+                rsync -av */${output} rsync://archive.intr/images/jenkins-production/
+            """
+        } else {
+            sh """
+                . /home/jenkins/.nix-profile/etc/profile.d/nix.sh ;
+                packer build -force -var-file=vars/${vars}.json templates/${template}.json
+                ls -alah */${output}
+                rsync -av */${output} rsync://archive.intr/images/jenkins-development/
+            """
+        }
+
+   }
 }
