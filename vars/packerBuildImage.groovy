c6d9c5c41fe029ad726bfbb31038a8cf55adb48a
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-08-20 15:19:25 +0300

    rm

--- vars/packerBuildImage.groovy
+++ vars/packerBuildImage.groovy
@@ -10,28 +10,29 @@ def call(Map args = [:]) {
 
     if(!upload){
     sh """
-        echo "vars: ${vars} templ: ${template} output: ${output}"
-        echo "args.vars: ${args.vars} args.templ: ${args.template} args.output: ${args.output}"
         . /home/jenkins/.nix-profile/etc/profile.d/nix.sh ;
         packer build -force -var-file=vars/${vars}.json templates/${template}.json
         ls -alah */${output}
-
     """
     }
     if(upload){
         if(env.BRANCH_NAME == "master"){
             sh """
+                rm -f */${output} || true
                 . /home/jenkins/.nix-profile/etc/profile.d/nix.sh ;
                 packer build -force -var-file=vars/${vars}.json templates/${template}.json
                 ls -alah */${output}
                 rsync -av */${output} rsync://archive.intr/images/jenkins-production/
+                rm -f */${output}
             """
         } else {
             sh """
+                rm -f */${output} || true
                 . /home/jenkins/.nix-profile/etc/profile.d/nix.sh ;
                 packer build -force -var-file=vars/${vars}.json templates/${template}.json
                 ls -alah */${output}
                 rsync -av */${output} rsync://archive.intr/images/jenkins-development/
+                rm -f */${output}
             """
         }
 
