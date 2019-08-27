19883d96868ab83ee938dac9d3b3b655454072b3
sidorov <sidorov@majordomo.ru>
2019-08-27 18:30:28 +0300

    buildWebService: withEnv

--- vars/nixBuildDocker.groovy
+++ vars/nixBuildDocker.groovy
@@ -10,7 +10,7 @@ def call(Map args = [:]) {
 
     createSshDirWithGitKey()
 
-    withEnv((['bash', '-c', "source /home/jenkins/.nix-profile/etc/profile.d/nix.sh && nix-shell -p docker --run env"].execute().text.split('\n'))) {
+    withEnv(['bash', '-c', "source /home/jenkins/.nix-profile/etc/profile.d/nix.sh && nix-shell -p docker --run env"].execute().text.split('\n')) {
         docker.withRegistry(registryUrl, credentialsId) {
             sh 'docker load --input $(nix-build --cores 8 --tarball-ttl 10 --show-trace)'
             sh 'tar xzf result manifest.json'
