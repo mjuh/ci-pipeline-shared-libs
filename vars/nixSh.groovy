4442330b9dfc6f3dc54a95a8c83e308ef2902a98
sidorov <sidorov@majordomo.ru>
2019-08-29 20:20:10 +0300

    java.util.List

--- vars/nixSh.groovy
+++ vars/nixSh.groovy
@@ -1,11 +1,11 @@
 def call(Map args = [:]) {
     assert args.cmd: "No cmd provided"
 
-    def env = ['sh', '-c', '. .nix-profile/etc/profile.d/nix.sh && env'].execute().text.trim().split('\n')
+    def env = ['sh', '-c', '. .nix-profile/etc/profile.d/nix.sh && env'].execute().text.trim().split('\n').collect {it}
     if (args.env) {
         env += args.env.collect { it }
     }
-    withEnv(env as java.util.List<java.lang.String>) {
+    withEnv(env>) {
         def pkgs = ['nix']
         if (args.pkgs) {
             pkgs += args.pkgs
