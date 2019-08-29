15fb312c1611724914071d5fdb0a19549692b8f2
sidorov <sidorov@majordomo.ru>
2019-08-29 19:59:07 +0300

    unwrap NixShell

--- vars/nixSh.groovy
+++ vars/nixSh.groovy
@@ -2,7 +2,7 @@ def call(Map args = [:]) {
     assert args.cmd : "No cmd provided"
 
     def home = new File(args.home ?: env.HOME)
-    def env = ['sh', '-c', '. .nix-profile/etc/profile.d/nix.sh && env'].execute(null, home).text.trim().split('\n')
+//    def env = ['sh', '-c', '. .nix-profile/etc/profile.d/nix.sh && env'].execute(null, home).text.trim().split('\n')
     def pkgs = ['nix']
     if (args.env) {
         env += args.env.collect { it }
@@ -11,5 +11,5 @@ def call(Map args = [:]) {
         pkgs += args.pkgs
     }
     def pkgStr = pkgs.collect { "-p ${it}" }.join(' ')
-    ['sh', '-c', "nix-shell --quiet ${pkgStr} --run '${args.cmd}'"].execute(env, home).text
+//    ['sh', '-c', "nix-shell --quiet ${pkgStr} --run '${args.cmd}'"].execute(env, home).text
 }
