9ff179eeacf5a7e4c4f180894182ecebd90f327d
sidorov <sidorov@majordomo.ru>
2019-08-29 20:01:47 +0300

    some debug printing

--- vars/nixSh.groovy
+++ vars/nixSh.groovy
@@ -2,6 +2,7 @@ def call(Map args = [:]) {
     assert args.cmd : "No cmd provided"
 
     def env = ['sh', '-c', '. .nix-profile/etc/profile.d/nix.sh && env'].execute().text.trim().split('\n')
+    println(env)
     def pkgs = ['nix']
     if (args.env) {
         env += args.env.collect { it }
