f635ce0176349494b5364d88eeb49df3d1a4d6ba
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-21 15:20:56 +0300

    vars: notifySlack: Update.

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,13 +1,32 @@
-def call(msg, color = 'green') {
-    def rgb = [green: '#00ff00',
-               yellow: '#ffff00',
-               red: '#ff0000',
-               good: '#00ff00',
-               bad: '#ff0000'][color.toLowerCase()]
-    slackSend(botUser: true,
-              channel: Constants.slackChannel,
-              color: rgb,
-              message: msg,
-              teamDomain: Constants.slackTeam,
-              token: Constants.slackToken)
+#!/usr/bin/env groovy
+
+// Original https://jenkins.io/blog/2017/02/15/declarative-notifications/
+
+/**
+ * Send notifications based on build status string
+ */
+def call(String buildStatus = 'STARTED') {
+    // build status of null means successful
+    buildStatus = buildStatus ?: 'SUCCESS'
+
+    // Default values
+    def colorName = 'RED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${JOB_NAME} [${BUILD_ID}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
+
+    // Override default values based on build status
+    if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+        colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESS') {
+        color = 'GREEN'
+        colorCode = '#00FF00'
+    } else {
+        color = 'RED'
+        colorCode = '#FF0000'
+    }
+
+    // Send notifications
+    slackSend (color: colorCode, message: summary)
 }
