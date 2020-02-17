4f29f2ab808c4f0e9fad1be98394d2378c54f3ba
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2020-02-17 17:11:59 +0000

    Packcer out

--- vars/packerBuildImage.groovy
+++ vars/packerBuildImage.groovy
@@ -1,13 +1,13 @@
 def call(Map args = [:]) {
         assert args.template : "No template provided"
-        assert args.output : "No output provided"
         assert args.vars : "No packervars provided"
 
-        def template = args.template
-        def output = args.output
         def vars = args.vars
         def upload = args.upload ?: false
-
+        def template = args.template
+        def output = args.output ?: { sh """
+                                       jq -r .vm_name vars/${vars}.json
+                                    """ }()
     if(!upload){
     sh """
         .  ${env.HOME}/.nix-profile/etc/profile.d/nix.sh || true ;
