24e27b27c12cffec5dd8a079f2737b639cde36f1
Александр Резвов <rezvov@majordomo.ru>
2020-02-20 13:54:32 +0000

    Something

--- src/DockerImageTarball.groovy
+++ src/DockerImageTarball.groovy
@@ -1,3 +1,6 @@
 class DockerImageTarball implements Serializable {
-    String imageName, path
+    String imageName, path;
+    public String imageName() {
+	return this.imageName.split("/").drop(1).join("/").split(":")[0];
+    }
 }
