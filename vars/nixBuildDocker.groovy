fa9d213cf6a16eb7aa3ef726b5ed20d782c4bdb4
Oleg Pykhalov <go.wigust@gmail.com>
2020-02-21 11:34:25 +0300

    vars: nixBuildDocker: Save out-link.
    * vars/buildWebService.groovy: Don't clean workspace on success.
    * vars/nixBuildDocker.groovy: Save out-link.

--- vars/nixBuildDocker.groovy
+++ vars/nixBuildDocker.groovy
@@ -6,7 +6,7 @@ def call(Map args = [:]) {
     Boolean debug = args.debug ?: false
     List<String> nixArgs = args.nixArgs ?: [""]
     String overlaybranch = args?.overlay?.branch ?: "master"
-    String buildCmd = "nix-build --no-out-link --tarball-ttl 10 --argstr ref $overlaybranch --show-trace"
+    String buildCmd = "nix-build --out-link result/${env.JOB_NAME}/${env.BUILD_NUMBER} --tarball-ttl 10 --argstr ref $overlaybranch --show-trace"
     String imageName = args.imageName ?: "${args.namespace}/${args.name}"
     String tag = args.tag ?: GIT_BRANCH
 
