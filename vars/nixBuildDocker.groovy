b501e45d5e3e195748c42ea3888b8eba743d7de6
Oleg Pykhalov <go.wigust@gmail.com>
2020-02-21 17:01:43 +0300

    vars: nixBuildDocker: Prefix result with 'docker-'.

--- vars/nixBuildDocker.groovy
+++ vars/nixBuildDocker.groovy
@@ -6,7 +6,7 @@ def call(Map args = [:]) {
     Boolean debug = args.debug ?: false
     List<String> nixArgs = args.nixArgs ?: [""]
     String overlaybranch = args?.overlay?.branch ?: "master"
-    String buildCmd = "nix-build --out-link result/${env.JOB_NAME}/${env.BUILD_NUMBER} --tarball-ttl 10 --argstr ref $overlaybranch --show-trace"
+    String buildCmd = "nix-build --out-link result/${env.JOB_NAME}/docker-${env.BUILD_NUMBER} --tarball-ttl 10 --argstr ref $overlaybranch --show-trace"
     String imageName = args.imageName ?: "${args.namespace}/${args.name}"
     String tag = args.tag ?: GIT_BRANCH
 
