04c63d266c0a5668a82301e5389932626298e8ff
Oleg Pykhalov <go.wigust@gmail.com>
2020-02-25 12:20:15 +0300

    vars: testNix: Save output link.

--- vars/testNix.groovy
+++ vars/testNix.groovy
@@ -1,6 +1,6 @@
 def call(Map args = [:]) {
     List<String> nixArgs = args.nixArgs ?: [""]
 
-    sh "nix-build test.nix ${nixArgs.join(' ')} --show-trace --out-link result-test"
+    sh "nix-build --out-link result/${env.JOB_NAME}/test-${env.BUILD_NUMBER} test.nix ${nixArgs.join(' ')} --show-trace --out-link result-test"
     archiveArtifacts (artifacts: "result-test/**")
 }
