d0927dc5fcd7223d80a669bf62d08df15f1feb34
Oleg Pykhalov <go.wigust@gmail.com>
2020-02-27 11:45:36 +0300

    vars: nixCleanWS: Fix.

--- vars/nixCleanWS.groovy
+++ vars/nixCleanWS.groovy
@@ -2,7 +2,7 @@ def call(Map args = [:]) {
     Number mtime = args.mtime ?: 7
     String directory = args.directory ?: "${env.WORKSPACE}/result"
     List<String> toDelete = "find $directory -type l ! -mtime -$mtime".execute().text.trim().split("\n")
-    if (toDelete) {
+    if (!toDelete.isEmpty()) {
         sh (toDelete.collect{"rm -f $it"}.join("; "))
     }
 }
