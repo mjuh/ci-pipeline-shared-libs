f5c19341f9ad3a58c796fb326e59b74e4b499019
Oleg Pykhalov <go.wigust@gmail.com>
2020-03-17 17:59:48 +0300

    vars: buildWebService: Add "Test Docker image without sandbox" stage.
    * vars/buildWebService.groovy ("Test Docker image without sandbox"):
    New stage.
    ("Test Docker image"): Specify nixFile.
    * vars/testNix.groovy (nixFile): New argument.

--- vars/testNix.groovy
+++ vars/testNix.groovy
@@ -1,6 +1,12 @@
 def call(Map args = [:]) {
+    assert args.nixFile : "No image name provided"
+
     List<String> nixArgs = args.nixArgs ?: [""]
 
-    sh "nix-build --out-link result/${env.JOB_NAME}/test-${env.BUILD_NUMBER} test.nix ${nixArgs.join(' ')} --show-trace --out-link result-test"
-    archiveArtifacts (artifacts: "result-test/**")
+    sh ([ "nix-build",
+          "--out-link",
+          "result/${env.JOB_NAME}/$args.nixFile-${env.BUILD_NUMBER}", "${args.nixFile}.nix",
+         nixArgs.join(' '), "--show-trace"].join(" "))
+
+    archiveArtifacts (artifacts: "result/${env.JOB_NAME}/$args.nixFile-${env.BUILD_NUMBER}/**")
 }
