2f60e971a64f2194bf9242154f4a8101adc0aef1
Oleg Pykhalov <go.wigust@gmail.com>
2020-04-07 10:17:36 +0300

    vars: packerBuildImages: Fix update collate.
    This commit follows db092536ac4e338790706ebc6d268b6c8ffa989d.

--- vars/packerBuildImages.groovy
+++ vars/packerBuildImages.groovy
@@ -63,8 +63,8 @@ def call(Map args = [:]) {
     List<String> nodeParameter = random(names) // will become an empty array
     Map stepsForParallel = [:]
 
-    def jobs = [[args.distribution], tarifs.keySet()].combinations()
-    jobs.collate((jobs.size >= args.nodeLabels.size ? jobs.size - args.nodeLabels.size() : 1) ?: concurrent)
+    def imagesVariations = [[args.distribution], tarifs.keySet()].combinations()
+    imagesVariations.collate((imagesVariations.size() >= args.nodeLabels.size() ? jobs.size - args.nodeLabels.size() : 1) ?: concurrent)
         .collect{ jobs ->
             if (nodeParameter != null && nodeParameter != []) {
                 firstNodeName = nodeParameter.head()
