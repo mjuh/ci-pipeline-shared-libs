a2fee097f9595bfea016d6ea0c31ff96e3dba3eb
Oleg Pykhalov <go.wigust@gmail.com>
2020-04-21 17:26:28 +0300

    vars: buildWebService: Move sendNotifications to separate file.
    * vars/buildWebService.groovy (sendNotifications): Move ... .
    * vars/sendSlackNotifications.groovy: ... here.

---
+++ vars/sendSlackNotifications.groovy
@@ -0,0 +1,37 @@
+/**
+ * Send notifications based on build status string.
+ * Thanks to https://jenkins.io/blog/2017/02/15/declarative-notifications/
+ */
+def call(Map args = [:]) {
+    String buildStatus = args.buildStatus ?: 'STARTED'
+
+    // build status of null means successful
+    buildStatus = buildStatus ?: 'SUCCESS'
+
+    // Default values
+    def colorName = 'RED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${JOB_NAME} [${BUILD_ID}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
+
+    // Override default values based on build status
+    if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+        colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESS') {
+        color = 'GREEN'
+        colorCode = '#00FF00'
+    } else {
+        color = 'RED'
+        colorCode = '#FF0000'
+    }
+
+    // Send notifications
+    if (args.threadMessages) {
+        slackSend (color: colorCode,
+                   message: ([summary,
+                              args.threadMessages.join("\n\n")].join("\n\n")))
+    } else {
+        slackSend (color: colorCode, message: summary)
+    }
+}
