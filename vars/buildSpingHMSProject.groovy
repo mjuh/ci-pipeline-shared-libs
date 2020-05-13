69a8c62e9e6718afa10145b3e7cee8c50b77b87c
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-13 13:44:03 +0300

    vars: buildSpingHMSProject: Refactor.
    * vars/buildSpingHMSProject.groovy: Indent.  Fix line width.  Use
    sendSlackNotifications.

--- vars/buildSpingHMSProject.groovy
+++ vars/buildSpingHMSProject.groovy
@@ -1,64 +1,86 @@
 def call() {
     def dockerImage = null
+    def slackMessages = [];
 
     pipeline {
-        agent { label 'master' }
-        parameters { string(name: 'dockerStacksRepoCommitId',
-                            defaultValue: '',
-                            description: "ID коммита в репозитории ${Constants.dockerStacksGitRepoUrl}, " +
-                                         "если оставить пустым, при деплое будет использован последний коммит в master")
-                     booleanParam(name: 'skipToDeploy',
-                                  defaultValue: false,
-                                  description: 'пропустить сборку и тестирование')
-                     booleanParam(name: 'switchStacks',
-                                  defaultValue: false,
-                                  description: 'Свичнуть стэки?') 
-                    }
+        agent { label "master" }
+        parameters {
+            string(
+                name: "dockerStacksRepoCommitId",
+                defaultValue: "",
+                description: "ID коммита в репозитории ${Constants.dockerStacksGitRepoUrl}, " +
+                    "если оставить пустым, при деплое будет использован последний коммит в master"
+            )
+            booleanParam(
+                name: "skipToDeploy",
+                defaultValue: false,
+                description: "пропустить сборку и тестирование"
+            )
+            booleanParam(
+                name: "switchStacks",
+                defaultValue: false,
+                description: "Свичнуть стэки?"
+            )
+        }
         environment {
             PROJECT_NAME = gitRemoteOrigin.getProject()
             GROUP_NAME = gitRemoteOrigin.getGroup()
-            INACTIVE_STACK = nginx.getInactive('/hms')
+            INACTIVE_STACK = nginx.getInactive("/hms")
         }
         options {
-            buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+            buildDiscarder(
+                logRotator(
+                    numToKeepStr: "10",
+                    artifactNumToKeepStr: "10"
+                )
+            )
             gitLabConnection(Constants.gitLabConnection)
-            gitlabBuilds(builds: ['Build Gradle', 'Push Docker image'])
+            gitlabBuilds(builds: [
+                    "Build Gradle",
+                    "Push Docker image"
+                ]
+            )
         }
-       tools {
-            gradle "latest"
-        }
+        tools { gradle "latest" }
         stages {
-            stage('Build Gradle') {
+            stage("Build Gradle") {
                 when {
                     allOf {
-                        not { expression { return params.skipToDeploy } }
-                        not { expression { return params.switchStacks } }
+                        not { expression { params.skipToDeploy } }
+                        not { expression { params.switchStacks } }
                     }
                 }
                 steps {
                     gitlabCommitStatus(STAGE_NAME) {
-                            sh ' gradle build ' 
+                        sh "gradle build"
                     }
                 }
             }
-            stage('Build Docker jdk image') {
+            stage("Build Docker jdk image") {
                 when {
                     allOf {
-                        expression { fileExists 'Dockerfile.jdk' }
-                        not { expression { return params.skipToDeploy } }
-                        not { expression { return params.switchStacks } }
+                        expression { fileExists "Dockerfile.jdk" }
+                        not { expression { params.skipToDeploy } }
+                        not { expression { params.switchStacks } }
                     }
                 }
                 steps {
-                        script { dockerImage = buildDocker namespace: GROUP_NAME, dockerfile: 'Dockerfile.jdk',name: PROJECT_NAME, tag: GIT_COMMIT[0..7]+'-jdk' }
+                    script {
+                        dockerImage = buildDocker (
+                            namespace: GROUP_NAME,
+                            dockerfile: "Dockerfile.jdk",
+                            name: PROJECT_NAME,
+                            tag: GIT_COMMIT[0..7] + "-jdk"
+                        )
+                    }
                 }
             }
-            stage('Push Docker jdk image') {
-               when {
+            stage("Push Docker jdk image") {
+                when {
                     allOf {
-                        expression { fileExists 'Dockerfile.jdk' }
-                        not { expression { return params.skipToDeploy } }
-                        not { expression { return params.switchStacks } }
+                        expression { fileExists "Dockerfile.jdk" }
+                        not { expression { params.skipToDeploy } }
+                        not { expression { params.switchStacks } }
                     }
                 }
                 steps {
@@ -67,25 +89,31 @@ def call() {
                     }
                 }
             }
-            stage('Build Docker jre image') {
+            stage("Build Docker jre image") {
                 when {
                     allOf {
-                        not { expression { return params.skipToDeploy } }
-                        not { expression { return params.switchStacks } }
+                        not { expression { params.skipToDeploy } }
+                        not { expression { params.switchStacks } }
                     }
                 }
                 steps {
                     gitlabCommitStatus(STAGE_NAME) {
-                        script { dockerImage = buildDocker namespace: GROUP_NAME, name: PROJECT_NAME, tag: GIT_COMMIT[0..7] }
+                        script {
+                            dockerImage = buildDocker (
+                                namespace: GROUP_NAME,
+                                name: PROJECT_NAME,
+                                tag: GIT_COMMIT[0..7]
+                            )
+                        }
                     }
                 }
             }
-            stage('Test Docker image structure') {
+            stage("Test Docker image structure") {
                 when {
                     allOf {
-                        expression { fileExists 'container-structure-test.yaml' }
-                        not { expression { return params.skipToDeploy } }
-                        not { expression { return params.switchStacks } }
+                        expression { fileExists "container-structure-test.yaml" }
+                        not { expression { params.skipToDeploy } }
+                        not { expression { params.switchStacks } }
                     }
                 }
                 steps {
@@ -94,43 +122,53 @@ def call() {
                     }
                 }
             }
-            stage('Push Docker image') {
-               when {
+            stage("Push Docker image") {
+                when {
                     allOf {
-                        not { expression { return params.skipToDeploy } }
-                        not { expression { return params.switchStacks } }
+                        not { expression { params.skipToDeploy } }
+                        not { expression { params.switchStacks } }
                     }
                 }
                 steps {
                     gitlabCommitStatus(STAGE_NAME) {
-                        pushDocker image: dockerImage
+                        script {
+                            pushDocker image: dockerImage
+                            String DOCKER_REGISTRY_BROWSER_URL = "${Constants.dockerRegistryBrowserUrl}/repo/${GROUP_NAME}/${PROJECT_NAME}/tag/${GIT_COMMIT[0..7]}"
+                            slackMessages += "<${DOCKER_REGISTRY_BROWSER_URL}|${DOCKER_REGISTRY_BROWSER_URL}>"
+                        }
                     }
                 }
             }
-            stage('Pull Docker image') {
-                when { 
-                    allOf { 
-                        branch 'master'
-                        not { expression { return params.switchStacks } } 
+            stage("Pull Docker image") {
+                when {
+                    allOf {
+                        branch "master"
+                        not { expression { params.switchStacks } }
                     }
-                }    
+                }
                 steps {
                     gitlabCommitStatus(STAGE_NAME) {
                         dockerPull image: dockerImage
                     }
                 }
             }
-            stage('Deploy service to swarm') {
-                when { 
-                    allOf { 
-                        branch 'master' 
-                        not { expression { return params.switchStacks } } 
+            stage("Deploy service to swarm") {
+                when {
+                    allOf {
+                        branch "master"
+                        not { expression { params.switchStacks } }
                     }
-                 }   
+                }
                 agent { label Constants.productionNodeLabel }
                 steps {
                     gitlabCommitStatus(STAGE_NAME) {
-                        dockerStackDeploy stack: INACTIVE_STACK, service: PROJECT_NAME, image: dockerImage, stackConfigFile: 'hms.yml', dockerStacksRepoCommitId: params.dockerStacksRepoCommitId
+                        dockerStackDeploy (
+                            stack: INACTIVE_STACK,
+                            service: PROJECT_NAME,
+                            image: dockerImage,
+                            stackConfigFile: "hms.yml",
+                            dockerStacksRepoCommitId: params.dockerStacksRepoCommitId
+                        )
                     }
                 }
                 post {
@@ -139,28 +177,32 @@ def call() {
                     }
                 }
             }
-            stage('Switch stacks') {
+            stage("Switch stacks") {
                 when {
                     allOf {
-                        branch 'master'
-                        expression { return params.switchStacks }
+                        branch "master"
+                        expression { params.switchStacks }
                     }
                 }
                 steps {
                     gitlabCommitStatus(STAGE_NAME) {
-                       script {nginx.Switch('/hms')}
-                    }
-                }
-                post {
-                    success {
-                        notifySlack "Switched to ${INACTIVE_STACK} stack"
+                        script {
+                            nginx.Switch("/hms")
+                            slackMessages +=
+                                "Switched to ${INACTIVE_STACK} stack"
+                        }
                     }
                 }
             }
         }
         post {
             success { cleanWs() }
-            failure { notifySlack "Build failled: ${JOB_NAME} [<${RUN_DISPLAY_URL}|${BUILD_NUMBER}>]", "red" }
+            always {
+                sendSlackNotifications (
+                    buildStatus: currentBuild.result,
+                    threadMessages: slackMessages
+                )
+            }
         }
     }
 }
