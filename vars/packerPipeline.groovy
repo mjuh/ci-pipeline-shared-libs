118e316162a378f05aa1c9d9ccfda389ce8b14dc
Oleg Pykhalov <go.wigust@gmail.com>
2021-01-28 17:46:58 +0300

    vars: packerPipeline: Reimplement using jsonnet and Nix.
    * vars/packerBuildImages.groovy: Delete file.
    * vars/packerPipeline.groovy: Reimplement using jsonnet and Nix.

--- vars/packerPipeline.groovy
+++ vars/packerPipeline.groovy
@@ -1,27 +1,27 @@
 def call(Map args = [:]) {
-    Boolean administration = args.administration ? args.administration : false
     pipeline {
-        agent { label "master" }
-        environment {
-            PACKER_LOG = "1"
-            PACKER_CACHE_DIR = "/tmp/packer"
-        }
+        agent { label "kvm-template-builder" }
         options {
             buildDiscarder(logRotator(numToKeepStr: "10", artifactNumToKeepStr: "10"))
             disableConcurrentBuilds()
             timeout(time: 6, unit: "HOURS")
         }
         stages {
-            stage("Build virtual machine") {
+            stage("Build") {
                 steps {
-                    warnError("Failed to build virtual machine") {
-                        packerBuildImages (
-                            distribution: args.distribution,
-                            release: args.release,
-                            id: args.id,
-                            administration: administration,
-                            nodeLabels: ["kvm-template-builder"]
-                        )
+                    script {
+                        flake = "show-packer-build" + "-" + args.distribution + "-" + args.release + (args.distribution == null ? "" : "-administration")
+                        (sh(returnStdout: true, script: nix.shell(run: String.format("nix run .#%s --impure", flake))).trim().split("\n")).each { command ->
+                            ansiColor("xterm") { sh(["PACKER_LOG=1", "PACKER_CACHE_DIR=/tmp/packer", command].join(" ")) } 
+                        }
+                    }
+                }
+            }
+            stage("Deploy") {
+                steps {
+                    script {
+                        sh String.format("rsync --archive --verbose --backup output/ %s",
+                                         ("rsync://archive.intr/images/" + (env.BRANCH_NAME == "master" ? "jenkins-production" : "jenkins-development") + "/"))
                     }
                 }
             }
@@ -34,3 +34,4 @@ def call(Map args = [:]) {
         }
     }
 }
+
