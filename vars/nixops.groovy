aecf6c6c86468d37409e9158e967a1c95a5ca487
Oleg Pykhalov <go.wigust@gmail.com>
2021-02-03 18:37:09 +0300

    vars: nixops: Fix args.

--- vars/nixops.groovy
+++ vars/nixops.groovy
@@ -2,5 +2,5 @@ def deploy(Map args = [:]) {
     (["nixops", "deploy", "--deployment", args.deployment]
      + (args.showTrace instanceof Boolean ? ["--show-trace"] : [])
      + (args.maxConcurrentActivate instanceof Number ? ["--max-concurrent-activate", args.maxConcurrentActivate] : [])
-     + args.args).join(" ")
+     + (args.args == null ? [] : args.args)).join(" ")
 }
