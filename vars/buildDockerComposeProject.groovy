8ebe1a881bc47dac2a0b3321713fca10fb9133f7
Oleg Pykhalov <go.wigust@gmail.com>
2021-02-15 12:40:08 +0300

    Revert "vars: buildDockerComposeProject: Add optional deployPhase argument."
    This reverts commit 6bd6a26af06e10d76368d56a6ab80627598d0f0c.

--- vars/buildDockerComposeProject.groovy
+++ vars/buildDockerComposeProject.groovy
@@ -72,21 +72,21 @@ def call(String composeProject, Map args = [:]) {
                     beforeAgent true
                 }
                 steps {
-                    script {
-                        defaultDeployPhase = {
-                            node(composeProject) {
-                                dockerComposeDeploy (
-                                    project: composeProject,
-                                    service: PROJECT_NAME,
-                                    image: dockerImage,
-                                    dockerStacksRepoCommitId: params.dockerStacksRepoCommitId,
-                                    projectConfigFile: "elk-" + env.NODE_NAME + ".yml"
-                                )
+                    gitlabCommitStatus(STAGE_NAME) {
+                        sequentialCall (
+                            nodeLabels: [composeProject],
+                            procedure: { nodeLabels ->
+                                ansiColor("xterm") {
+                                    dockerComposeDeploy (
+                                        project: composeProject,
+                                        service: PROJECT_NAME,
+                                        image: dockerImage,
+                                        dockerStacksRepoCommitId: params.dockerStacksRepoCommitId,
+                                        projectConfigFile: "elk-" + env.NODE_NAME + ".yml"
+                                    )
+                                }
                             }
-                        }
-                        gitlabCommitStatus(STAGE_NAME) {
-                            args.deployPhase == null ? defaultDeployPhase() : args.deployPhase()
-                        }
+                        )
                     }
                 }
                 post {
