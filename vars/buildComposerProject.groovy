afa4837882606cb277475862bde2757f192cb9c0
Oleg Pykhalov <go.wigust@gmail.com>
2021-08-17 15:46:15 +0300

    vars: composer: Migrate to gitlab-branch-source.
    This commit follows 67b2fd3773a49d291cb8fdd1082661c5bfb68360.

--- vars/buildComposerProject.groovy
+++ vars/buildComposerProject.groovy
@@ -10,7 +10,9 @@ def call(String phpVersion) {
             stage('Install PHP dependencies with Composer') {
                 steps {
                     println "id".execute().text
-                    composer phpVersion: phpVersion
+                    composer (phpVersion: phpVersion
+                              GITLAB_PROJECT_NAMESPACE: GITLAB_PROJECT_NAMESPACE,
+                              GITLAB_PROJECT_NAME: GITLAB_PROJECT_NAME)
                 }
             }
             stage('Build Docker image') {
