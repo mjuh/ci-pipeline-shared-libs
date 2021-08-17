509e80a754216dd55d6dcd8b7423c67ae566dca0
Oleg Pykhalov <go.wigust@gmail.com>
2021-08-17 15:42:16 +0300

    vars: composer: Migrate to gitlab-branch-source.

--- vars/composer.groovy
+++ vars/composer.groovy
@@ -39,7 +39,7 @@ def call(Map args = [:]) {
                     COMPOSER_HOME: '/composer/home',
                     GIT_SSH: "${jenkinsHomeInComposerContainer}/.ssh/ssh_wrapper.sh"],
               image: "${composerImage}:${composerTag}",
-              name: "composer-${env.BUILD_TAG}",
+              name: "composer-jenkins-${GITLAB_PROJECT_NAMESPACE}-${GITLAB_PROJECT_NAME}-${env.BUILD_NUMBER}",
               cmd: cmd)
     echo 'Removing composer.lock for next build'
     sh "rm -f $WORKSPACE/build/composer.lock"
