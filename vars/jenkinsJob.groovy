e1593151720f30cf0f22335bcfa94c9c61d37fb1
Oleg Pykhalov <go.wigust@gmail.com>
2021-08-17 17:34:26 +0300

    vars: buildComposerProject: Migrate to gitlab-branch-source.

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -1,7 +1,7 @@
-def getProject() {
-    env.JOB_NAME.split("%2F")[1].split("/")[0]
+def getProject(jobName) {
+    jobName.split("%2F")[1].split("/")[0]
 }
 
-def getGroup() {
-    env.JOB_NAME.split("%2F")[0].split("/")[0]
+def getGroup(jobName) {
+    jobName.split("%2F")[0].split("/")[0]
 }
