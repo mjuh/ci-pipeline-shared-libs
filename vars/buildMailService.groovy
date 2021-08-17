c74c19e11945bd57a32b71822c82ccd261654e31
Oleg Pykhalov <go.wigust@gmail.com>
2021-08-17 22:57:35 +0300

    vars: buildMailService: Migrate to gitlab-branch-source.

--- vars/buildMailService.groovy
+++ vars/buildMailService.groovy
@@ -3,13 +3,11 @@ def call(Map args = [:]) {
     pipeline {
         agent { label "nixbld" }
         options {
-            gitLabConnection(library("mj-shared-library").Constants.gitLabConnection)
-            gitlabBuilds(builds: ["Build Docker image", "Test", "Push Docker image"])
             buildDiscarder(logRotator(numToKeepStr: "10", artifactNumToKeepStr: "10"))
         }
         environment {
-            PROJECT_NAME = gitRemoteOrigin.getProject()
-            GROUP_NAME = gitRemoteOrigin.getGroup()
+            PROJECT_NAME = jenkinsJob.getProject(env.JOB_NAME)
+            GROUP_NAME = jenkinsJob.getGroup(env.JOB_NAME)
         }
         parameters {
             booleanParam(
@@ -21,47 +19,47 @@ def call(Map args = [:]) {
         stages {
             stage("Build Docker image") {
                 steps {
-                    gitlabCommitStatus(STAGE_NAME) {
-                        script {
-                            dockerImage = nixBuildDocker (
-                                namespace: GROUP_NAME, 
-                                name: PROJECT_NAME, 
-                                currentProjectBranch: GIT_BRANCH,
-                                overlaybranch: "master"
-                            )
-                        }
-                    }
-                }
-            }
-            stage("Test") {
-                when { not { expression { params.skipToDeploy } } }
-                steps {
-                    gitlabCommitStatus(STAGE_NAME) {
-                        build (
-                            job: "../mail-tests/master",
-                            parameters: [
-                                string(name: "TARGET_PROJECT", value: PROJECT_NAME),
-                                string(name: "TARGET_BRANCH", value: GIT_BRANCH)
-                            ]
+                    script {
+                        dockerImage = nixBuildDocker (
+                            namespace: GROUP_NAME, 
+                            name: PROJECT_NAME, 
+                            currentProjectBranch: GIT_BRANCH,
+                            overlaybranch: "master"
                         )
                     }
                 }
             }
+            stage("Test") {
+                when {
+                    not { expression { params.skipToDeploy } }
+                    beforeAgent true
+                }
+                steps {
+                    build (
+                        job: "../mail-tests/master",
+                        parameters: [
+                            string(name: "TARGET_PROJECT", value: PROJECT_NAME),
+                            string(name: "TARGET_BRANCH", value: GIT_BRANCH)
+                        ]
+                    )
+                }
+            }
             stage("Push Docker image") {
                 steps {
-                    gitlabCommitStatus(STAGE_NAME) {
-                        script {
-                            pushDocker (
-                                image: dockerImage,
-                                pushToBranchName: false
-                            )
-                            slackMessages += "${GROUP_NAME}/${PROJECT_NAME}:${GIT_BRANCH} pushed to registry"
-                        }
+                    script {
+                        pushDocker (
+                            image: dockerImage,
+                            pushToBranchName: false
+                        )
+                        slackMessages += "${GROUP_NAME}/${PROJECT_NAME}:${GIT_BRANCH} pushed to registry"
                     }
                 }
             }
             stage("Deploy") {
-                when { branch "master" }
+                when {
+                    branch "master"
+                    beforeAgent true
+                }
                 steps {
                     script {
                         (args.deploy ?: { return false })([input: [
