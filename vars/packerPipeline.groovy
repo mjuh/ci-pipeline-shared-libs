0825dc3dca30523ff6a947e31866a10c41755b5a
Oleg Pykhalov <go.wigust@gmail.com>
2021-08-24 17:22:41 +0300

    vars: packerPipeline: Run Nix flake via HTTPs.

--- vars/packerPipeline.groovy
+++ vars/packerPipeline.groovy
@@ -13,7 +13,7 @@ def call(Map args = [:]) {
                         flake = "show-packer-build" + "-" + args.distribution + "-" + args.release + (args.administration == null ? "" : "-administration")
                         (sh(returnStdout: true,
                             script: """
-                                    nix-shell --run 'nix run \".#\\\"${flake}\\\"\" --impure'
+                                    nix-shell --packages nixUnstable --run 'nix run \"${kvmTemplates}#\\\"${flake}\\\"\" --impure'
                                     """).trim().split("\n")).each { command ->
                                                                     ansiColor("xterm") {
                                                                          sh(["PACKER_LOG=1", "PACKER_CACHE_DIR=/tmp/packer", command].join(" "))
