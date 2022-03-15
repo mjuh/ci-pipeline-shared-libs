f6a926e18fdf1a2a6a1e4b17de107af456c71566
Oleg Pykhalov <go.wigust@gmail.com>
2022-03-15 20:25:33 +0300

    vars: configureGit: Do not invoke createSshDirWithGitKey.
    /var/lib/jenkins/.ssh/config is read-only managed by Nix.

--- vars/configureGit.groovy
+++ vars/configureGit.groovy
@@ -1,5 +1,4 @@
 def call(Map args = [:]) {
-    createSshDirWithGitKey(dir: HOME + '/.ssh')
     sh """
           git config --global user.name 'jenkins'
           git config --global user.email 'jenkins@majordomo.ru'
