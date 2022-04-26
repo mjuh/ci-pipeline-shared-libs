989e2f229156cf9257f841dce6071274176a615e
Oleg Pykhalov <go.wigust@gmail.com>
2022-04-26 15:46:49 +0300

    src: DockerImageTarball: Do not drop tag name.

--- src/DockerImageTarball.groovy
+++ src/DockerImageTarball.groovy
@@ -1,6 +1,6 @@
 class DockerImageTarball implements Serializable {
     String imageName, path;
     public String imageName() {
-	return this.imageName.split("/").drop(1).join("/").split(":")[0];
+	return this.imageName.split("/").drop(1).join("/");
     }
 }
