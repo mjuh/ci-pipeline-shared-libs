5daabb3de4df7aa91606f156656ec3ff2e226185
Oleg Pykhalov <go.wigust@gmail.com>
2022-09-14 23:41:39 +0300

    vars/nginx.groovy

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -57,9 +57,10 @@ def Switch(String apipath) {
     // define the secrets and the env variables
     // engine version can be defined on secret, job, folder or global.
     // the default is engine version 2 unless otherwise specified globally.
-    def secrets = [[path: 'secret/admin.majordomo.ru/admin',
+    def secrets = [[path: 'secret/vaultPass/majordomo/nginx1.intr',
                     engineVersion: 2,
-                    secretValues: [[vaultKey: 'username'], [vaultKey: 'password']]]]
+                    secretValues: [[vaultKey: 'username'],
+                                   [vaultKey: 'password']]]]
 
     // inside this block your credentials will be available as env variables
     withVault([vaultSecrets: secrets]) {
