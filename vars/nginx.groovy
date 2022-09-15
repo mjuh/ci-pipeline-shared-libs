064a711468d8504dc24c5c876eeccf7bed3f4161
Oleg Pykhalov <go.wigust@gmail.com>
2022-09-15 05:47:18 +0300

    vars/nginx.groovy

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -35,28 +35,29 @@ def check(String apipath) {
 }
 
 def foo() {
+    check(apipath)
+    json = JsonOutput.toJson([setActive: getInactive(apipath)])
     def nginx = new RESTClient(Constants.nginx1ApiUrl)
+    nginx.auth.basic Constants.nginxAuthUser, Constants.nginxAuthPass
+    // def resp = nginx.post(
+    //         path: apipath,
+    //         body: json,
+    //         requestContentType: URLENC)
+    // assert resp.status == 200
+    println(nginx.get(path: apipath, requestContentType: URLENC).data)
+    println(nginx.dump())
+    // println(credentials('nginx-auth-pass').dump())
+    nginx = new RESTClient(Constants.nginx2ApiUrl)
+    nginx.auth.basic Constants.nginxAuthUser, Constants.nginxAuthPass
+    // resp = nginx.post(
+    //         path: apipath,
+    //         body: json,
+    //         requestContentType: URLENC)
+    // assert resp.status == 200
+    check(apipath)
 }
 
 def Switch(String apipath) { 
-    check(apipath)
-    json = JsonOutput.toJson([setActive: getInactive(apipath)])
-
-    // def nginx = new RESTClient(Constants.nginx1ApiUrl)
-    // nginx.auth.basic Constants.nginxAuthUser, Constants.nginxAuthPass
-
-    // // def resp = nginx.post(
-    // //         path: apipath,
-    // //         body: json,
-    // //         requestContentType: URLENC)
-
-    // // assert resp.status == 200
-
-    // println(nginx.get(path: apipath, requestContentType: URLENC).data)
-
-    // println(nginx.dump())
-
-
     // define the secrets and the env variables
     // engine version can be defined on secret, job, folder or global.
     // the default is engine version 2 unless otherwise specified globally.
@@ -64,26 +65,9 @@ def Switch(String apipath) {
                     engineVersion: 2,
                     secretValues: [[vaultKey: 'username'],
                                    [vaultKey: 'password']]]]
-
     // inside this block your credentials will be available as env variables
     withVault([vaultSecrets: secrets]) {
         println(username)
         println(password)
     }
-
-
-    // // println(credentials('nginx-auth-pass').dump())
-
-    // nginx = new RESTClient(Constants.nginx2ApiUrl)
-    // nginx.auth.basic Constants.nginxAuthUser, Constants.nginxAuthPass
-
-    // // resp = nginx.post(
-    // //         path: apipath,
-    // //         body: json,
-    // //         requestContentType: URLENC)
-
-    // // assert resp.status == 200
-
-    // check(apipath)
 }
-
