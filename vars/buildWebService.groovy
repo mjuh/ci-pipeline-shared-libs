3e24278091f16a204c0f8a682410880be9d5776c
Oleg Pykhalov <go.wigust@gmail.com>
2023-01-11 10:35:05 +0300

    vars: buildWebService: Run container structure test.

--- vars/buildWebService.groovy
+++ vars/buildWebService.groovy
@@ -19,6 +19,8 @@ def call(Map args = [:]) {
             stage("build") {
                 steps {
                     script {
+                        imageName = Constants.dockerRegistryHost + "/" + GITLAB_PROJECT_NAMESPACE + "/" + GITLAB_PROJECT_NAME + ":" + gitCommit().take(8)
+
                         (args.preBuild ?: { return true })()
 
                         nixFlakeLockUpdate (inputs: ["ssl-certificates"])
@@ -28,9 +30,10 @@ def call(Map args = [:]) {
                                 args.buildPhase(args)
                             }
                         } else {
+                            result = "result/${env.JOB_NAME}/docker-${env.BUILD_NUMBER}"
                             sh (nix.shell (run: ((["nix", "build"]
                                                   + Constants.nixFlags
-                                                  + ["--out-link", "result/${env.JOB_NAME}/docker-${env.BUILD_NUMBER}", ".#container"]
+                                                  + ["--out-link", result, ".#container"]
                                                   + (args.nixArgs == null ? [] : args.nixArgs)).join(" "))))
                         }
                     }
@@ -42,13 +45,23 @@ def call(Map args = [:]) {
                         if (args.tests == false) {
                             return 0
                         }
+                        (args.preTest ?: { return true })([result: result, imageName: imageName])
                         parallel (["nix flake check": {
-                            ansiColor("xterm") {
-                                sh (nix.shell (run: ((["nix flake check"]
-                                                      + Constants.nixFlags
-                                                      + (args.nixArgs == null ? [] : args.nixArgs)
-                                                      + (args.printBuildLogs == true ? ["--print-build-logs"] : [])
-                                                      + (args.showTrace == true ? ["--show-trace"] : [])).join(" "))))}}]
+                                     ansiColor("xterm") {
+                                         sh (nix.shell (run: ((["nix flake check"]
+                                                               + Constants.nixFlags
+                                                               + (args.nixArgs == null ? [] : args.nixArgs)
+                                                               + (args.printBuildLogs == true ? ["--print-build-logs"] : [])
+                                                               + (args.showTrace == true ? ["--show-trace"] : [])).join(" "))))}},
+                                   "container structure test": {
+                                      if (fileExists ("container-structure-test.yaml")) {
+                                          containerStructureTest (namespace: GITLAB_PROJECT_NAMESPACE,
+                                                                  name: GITLAB_PROJECT_NAME,
+                                                                  tag: gitCommit().take(8))
+                                      } else {
+                                          return true
+                                      }
+                                    }]
                                   + (args.scanPasswords == true ?
                                      ["bfg": {
                                     build (job: "../../${Constants.bfgJobName}/master",
@@ -77,7 +90,7 @@ def call(Map args = [:]) {
                             slackMessages += "<${DOCKER_REGISTRY_BROWSER_URL}|${DOCKER_REGISTRY_BROWSER_URL}>"
 
                             dockerImage = new DockerImageTarball(
-                                imageName: (Constants.dockerRegistryHost + "/" + GITLAB_PROJECT_NAMESPACE + "/" + GITLAB_PROJECT_NAME + ":" + gitCommit().take(8)),
+                                imageName: imageName,
                                 path: "" // XXX: Specifiy path in DockerImageTarball for flake buildWebService.
                             )
 
