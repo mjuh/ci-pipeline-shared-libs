ff10e60ba1a09f82fa839bed889292a303feb198
Oleg Pykhalov <go.wigust@gmail.com>
2023-02-19 23:02:46 +0300

    vars/buildWebService.groovy

--- vars/buildWebService.groovy
+++ vars/buildWebService.groovy
@@ -20,25 +20,7 @@ def call(Map args = [:]) {
         stages {
             stage("build") {
                 steps {
-                    script {
-                        imageName = Constants.dockerRegistryHost + "/" + GITLAB_PROJECT_NAMESPACE + "/" + GITLAB_PROJECT_NAME + ":" + gitCommit().take(8)
-
-                        (args.preBuild ?: { return true })()
-
-                        nixFlakeLockUpdate (inputs: ["ssl-certificates"])
-
-                        if (args.buildPhase) {
-                            ansiColor("xterm") {
-                                args.buildPhase(args)
-                            }
-                        } else {
-                            result = "result/${env.JOB_NAME}/docker-${env.BUILD_NUMBER}"
-                            sh (nix.shell (run: ((["nix", "build"]
-                                                  + Constants.nixFlags
-                                                  + ["--out-link", result, ".#container"]
-                                                  + (args.nixArgs == null ? [] : args.nixArgs)).join(" "))))
-                        }
-                    }
+                    sh "true"
                 }
             }
         }
