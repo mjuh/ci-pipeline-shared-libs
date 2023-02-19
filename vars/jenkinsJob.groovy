5cd537fd074f4b1381dccde717f3fc3cf27659e9
Oleg Pykhalov <go.wigust@gmail.com>
2023-02-19 23:09:43 +0300

    vars/jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -1,7 +1,7 @@
 def getProject(jobName) {
-    jobName.split("%2F")[1].split("/")[0]
+    jobName.split("/")[1]
 }
 
 def getGroup(jobName) {
-    jobName.split("%2F")[0].split("/")[0]
+    jobName.split("/")[0]
 }
